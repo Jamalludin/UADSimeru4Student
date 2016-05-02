6aadd2fdf60b0e6e26dcfd71c2d3bbc374e031ca
Jamalludin <jamalludin1209@gmail.com>
2016-05-02 21:42:25 +0700

    Add Spinner on JadwalkuliahActivity and JadwalRuangActivity

--- app/src/main/java/com/android/jamalludin/uadsimeru4student/JadwalKuliahActivity.java
+++ app/src/main/java/com/android/jamalludin/uadsimeru4student/JadwalKuliahActivity.java
@@ -3,16 +3,83 @@ package com.android.jamalludin.uadsimeru4student;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jamal on 12/04/16.
  */
-public class JadwalKuliahActivity extends AppCompatActivity{
+public class JadwalKuliahActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_jadwalkuliah);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Pilih Fakultas");
+        categories.add("Fakultas Ekonomi");
+        categories.add("Fakultas Hukum");
+        categories.add("Fakultas Farmasi");
+        categories.add("Fakultas Keguruan Dan Ilmu Pendidikan");
+        categories.add("Fakultas Kesehatan Masyarakat");
+        categories.add("Fakultas Psikologi");
+        categories.add("Fakultas Sastra");
+        categories.add("Fakultas Teknologi Industri");
+        categories.add("Fakultas Matematika Dan Ilmu Pengetahuan");
+        categories.add("Pascasarjana Teknik");
+        categories.add("Fakultas Tarbiyah Dirasat Islamiyah");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        // Spinner click listener
+        spinner2.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories2 = new ArrayList<String>();
+        categories2.add("Program Studi");
+        categories2.add("Fakultas Ekonomi");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner2.setAdapter(dataAdapter2);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        //Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/android/jamalludin/uadsimeru4student/JadwalRuangActivity.java
+++ app/src/main/java/com/android/jamalludin/uadsimeru4student/JadwalRuangActivity.java
@@ -3,15 +3,54 @@ package com.android.jamalludin.uadsimeru4student;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jamal on 12/04/16.
  */
-public class JadwalRuangActivity extends AppCompatActivity {
+public class JadwalRuangActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwalruang);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        // Spinner click listener
+        spinner3.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories3 = new ArrayList<String>();
+        categories3.add("Pilih Kampus");
+        categories3.add("Kampus I");
+        categories3.add("Kampus II");
+        categories3.add("Kampus III");
+        categories3.add("Kampus IV");
+        categories3.add("Kampus V");
+        categories3.add("Kampus VI");
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories3);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner3.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
